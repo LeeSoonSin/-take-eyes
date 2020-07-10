df917c1c24a4858f8c5d51956c4a85b46639f2ea
workantL <passant321@naver.com>
2020-07-10 11:47:35 +0900

    캐릭터 움직임과 도트 타일

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float Speed =2;
+    Rigidbody2D rigid;
+    float h;
+    float v;
+    bool isHorizonMove;
+    
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if(hDown|| vUp)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown||hUp)
+        {
+            isHorizonMove = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
+    }
+}
