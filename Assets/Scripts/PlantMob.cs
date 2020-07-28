e0477eac7062d852ee3fcc7077f7adbb47596bff
workantL <passant321@naver.com>
2020-07-29 00:31:40 +0900

    맵 타일 변경, 3층 이동 구현, 몹들 추가

---
+++ Assets/Scripts/PlantMob.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlantMob : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float speed;
+    bool isLeft =true;
+    private void Start()
+    {
+    
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag =="Border")
+        {
+            if(isLeft)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                isLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                isLeft = true;
+            }
+        }
+    }
+}
