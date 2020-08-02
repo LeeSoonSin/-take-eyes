50fa9d30369572f52bcf565f841f5dd66e3b2019
LeeSoonSin <67868140+LeeSoonSin@users.noreply.github.com>
2020-08-02 19:19:25 +0900

    칠판 수정
    Inspector만 수정해봤는데 되더라! 코드 문제는 없었어!

--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -27,8 +27,8 @@ public class TalkManager : MonoBehaviour
     // Update is called once per frame
     void GernerateData()
     {
-        talkData.Add(12, new string[] { "다른 애들은 어디로 간 거지?",
-            "왜 우리 둘만 여기에 있는 것일까?" });
+        talkData.Add(12, new string[] { "나는 빡빡이다",
+            "나는 빡빡이다!!!" });
         talkData.Add(1, new string[] { "오래된 칠판이다. 분필자국이 아직 남아있다." });
     }
 
