63b70288f4e698b3ae7324e8301faab0aa123507
workantL <passant321@naver.com>
2020-08-04 14:16:48 +0900

    2층 맵 구현

---
+++ Assets/Scripts/AvoidCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidCollision : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    void Update()
+    {
+        this.transform.position += transform.up * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Border"))
+        {
+            this.transform.Rotate(transform.rotation.x, transform.rotation.y, transform.rotation.z - 90);
+        }
+    }
+}
