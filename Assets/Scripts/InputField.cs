e263a92c99ce21685b070ee840a29ed29efb36e5
workantL <passant321@naver.com>
2020-08-10 10:28:48 +0900

    텍스트 창 입력

---
+++ Assets/Scripts/InputField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputField : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log(text.text);
+            Destroy(this.gameObject);
+             
+        }
+    }
+}
