871efbc60338c461c34f095c9057ab300cd773f1
workantL <passant321@naver.com>
2020-08-12 14:20:42 +0900

    코드 추가, 선택지 시스템 추가

--- Assets/Scripts/InputField.cs
+++ Assets/Scripts/InputField.cs
@@ -5,12 +5,24 @@ using UnityEngine.UI;
 
 public class InputField : MonoBehaviour
 {
+    public GameObject go;
+    public Animator anim;
     public Text text;
+    public bool choicelng;
     void Start()
     {
         
     }
-
+    public void StartAnswer()
+    {
+        go.SetActive(true);
+        anim.SetBool("Appear", true);
+    }
+    public void EndAnswer()
+    {
+        anim.SetBool("Appear", false);
+        go.SetActive(false);
+    }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Return))
---
+++ Assets/Scripts/Question_Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question_Test : MonoBehaviour
+{
+    [SerializeField]
+    public InputField answer;
+
+    public bool flag;
+    public int correctNumber;
+    private void Start()
+    {
+        answer = FindObjectOfType<InputField>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            StartCoroutine(ACoroutine());
+        }
+    }
+    IEnumerator ACoroutine()
+    {
+        answer.StartAnswer();
+        yield return new WaitUntil(() => !answer.choicelng);
+        Debug.Log(answer.text.text);
+    }
+}
