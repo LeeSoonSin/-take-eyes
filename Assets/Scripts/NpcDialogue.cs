edb49fd21cce41c7fd5c866aa6a6210c9bc8b561
LeeSoonSin <67868140+LeeSoonSin@users.noreply.github.com>
2020-08-19 23:44:42 +0900

    대화창 조금 추가

---
+++ Assets/Scripts/NpcDialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDialogue : MonoBehaviour
+{
+    public Dialogue dialogue_1;
+    public int DataNum;
+    private DialogueManager theDM;
+    public DataBase thedata;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theDM = FindObjectOfType<DialogueManager>();
+        thedata = FindObjectOfType<DataBase>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(Input.GetKey(KeyCode.Z) && thedata.switches[DataNum] == false)
+        {
+            theDM.ShowDialogue(dialogue_1);
+            thedata.switches[DataNum] = true;
+        }
+    }
+}
